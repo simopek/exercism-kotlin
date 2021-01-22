61418f14c8129642f65a46cb00fdde8d34d6db61
simopek <>
2021-01-22 22:42:51 +0100

    Complete resistor-color exercise

--- resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
+++ resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
@@ -1,6 +1,13 @@
 object ResistorColorDuo {
 
+    private val colorValues = listOf(
+        Color.BLACK, Color.BROWN, Color.RED,
+        Color.ORANGE, Color.YELLOW, Color.GREEN,
+        Color.BLUE, Color.VIOLET, Color.GREY, Color.WHITE
+    )
+
     fun value(vararg colors: Color): Int {
-        TODO("Implement this to complete the task")
+        return colors.slice(0..1).map { color -> colorValues.indexOf(color) }
+            .fold(0) { acc, nextColorValue -> acc * 10 + nextColorValue }
     }
 }
