e3e4ca5a50996d75f404fa5b8cb208c00d8b9e0c
simopek <>
2021-01-23 23:43:19 +0100

    Complete transpose exercise

--- transpose/src/main/kotlin/Transpose.kt
+++ transpose/src/main/kotlin/Transpose.kt
@@ -1,6 +1,18 @@
 object Transpose {
 
     fun transpose(input: List<String>): List<String> {
-        TODO("Implement this to complete the task")
+
+        val maxPadding = input.size
+        val maxLength = input.map { it.length }.max() ?: return emptyList()
+
+        val output = mutableListOf<String>()
+        (0 until maxLength).forEach { ith ->
+
+            val newString =
+                input.filter { str -> ith <= str.length - 1 }.map { str -> str[ith] }.joinToString(separator = "")
+            output.add(newString.padStart(maxPadding,' '))
+        }
+
+        return output
     }
 }
