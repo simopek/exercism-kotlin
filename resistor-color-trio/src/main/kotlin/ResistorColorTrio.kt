b06b56c3de871bfb685df3b69fe7708f36e03a96
simopek <>
2021-01-23 22:42:59 +0100

    Complete resistor-color-trio exercise

--- resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
+++ resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
@@ -1,6 +1,36 @@
+import java.text.DecimalFormat
+import kotlin.math.floor
+
 object ResistorColorTrio {
 
+    private val unitList =
+        listOf(Unit.OHMS, Unit.KILOOHMS, Unit.MEGAOHMS, Unit.GIGAOHMS, Unit.TERAOHMS, Unit.PETAOHMS, Unit.EXAOHMS)
+
     fun text(vararg input: Color): String {
-        TODO("Implement this to complete the task")
+
+        // we compute the base number from the first 2 bands
+        var base: Double = input.take(2).map { it.ordinal }.fold(0) { acc, nextColorValue -> acc * 10 + nextColorValue }.toDouble()
+        val baseGte10 = base >= 10
+        // we get the 3rd band telling us the exponent
+        val zerosBand = input[2]
+
+        var zerosToBeAdded = zerosBand.ordinal
+
+        var unit = Unit.OHMS
+
+        // if we "miss" only one digit so that we can use the "next" unit and the base is >= 10
+        // we use the next unit and we divide the base by 10
+        if(zerosToBeAdded % 3 == 2 && baseGte10) {
+            unit = unitList[floor((zerosBand.ordinal.toDouble() + 1) / 3).toInt()]
+            base /= 10
+            zerosToBeAdded = 0
+        } else {
+            unit = unitList[floor(zerosBand.ordinal.toDouble() / 3).toInt()]
+        }
+
+        var zeros = (zerosToBeAdded % 3).downTo(1).fold("") { acc, _ -> "${acc}0" }
+
+        // we format the base in case we don't have decimal points
+        return "${DecimalFormat("0.#").format(base)}$zeros ${unit.name.toLowerCase()}"
     }
 }
