ec5b4b81b8f05d721f7663ebe88eda7b4bc484d4
simopek <>
2021-01-26 22:34:03 +0100

    Complete darts exercise

--- darts/src/main/kotlin/Darts.kt
+++ darts/src/main/kotlin/Darts.kt
@@ -1,6 +1,22 @@
+import kotlin.math.sqrt
+
 object Darts {
 
-    fun score(x: Any, y: Any /* choose proper types! */): Int {
-        TODO("Implement this function to complete the task")
+    private const val OUTER_CIRCLE_RADIUS = 10
+    private const val MIDDLE_CIRCLE_RADIUS = 5
+    private const val INNER_CIRCLE_RADIUS = 1
+
+    fun score(x: Number, y: Number): Int {
+
+        val radius = computeRadius(x.toDouble(), y.toDouble())
+
+        return when {
+            radius <= INNER_CIRCLE_RADIUS -> 10
+            radius <= MIDDLE_CIRCLE_RADIUS -> 5
+            radius <= OUTER_CIRCLE_RADIUS -> 1
+            else -> 0
+        }
     }
+
+    private fun computeRadius(x: Double, y: Double): Double = sqrt(x * x + y * y)
 }
