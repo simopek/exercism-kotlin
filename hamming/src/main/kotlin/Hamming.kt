bc0391b81d56f08b12c57df4463b2cebff719682
simopek <>
2021-02-02 22:27:33 +0100

    Complete hamming exercise

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -1,6 +1,16 @@
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        TODO("Implement this function to complete the task")
+
+        require(leftStrand.length == rightStrand.length) {
+            "left and right strands must be of equal length"
+        }
+
+        return leftStrand.foldIndexed(0, {idx, acc, ch ->
+            when (rightStrand[idx]) {
+                ch -> acc
+                else -> acc + 1
+            }
+        })
     }
 }
