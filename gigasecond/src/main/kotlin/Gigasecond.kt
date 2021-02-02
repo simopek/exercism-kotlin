7fa5d330816f1986c2ee78ccfc2d149117f3838d
simopek <>
2021-02-02 23:05:49 +0100

    Complete gigasecond exercise

--- gigasecond/src/main/kotlin/Gigasecond.kt
+++ gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,8 +1,15 @@
+import java.time.Instant
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+import java.time.temporal.Temporal
+import java.time.temporal.TemporalUnit
 
-class Gigasecond {
+class Gigasecond(private val instant: Temporal) {
 
-    // TODO: Implement proper constructor
 
-    val date: LocalDateTime = TODO("Implement this getter to complete the task")
+    val date: LocalDateTime = when (instant) {
+        is LocalDate -> instant.atStartOfDay().plus(1000000000, ChronoUnit.SECONDS)
+        else -> LocalDateTime.from(instant.plus(1000000000, ChronoUnit.SECONDS))
+    }
 }
