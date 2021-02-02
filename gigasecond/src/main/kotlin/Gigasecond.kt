f4589c6c8094850b1e993eb64d22c4f49d516b7d
simopek <>
2021-02-02 23:10:39 +0100

    Improve solution to gigasecond exercise

--- gigasecond/src/main/kotlin/Gigasecond.kt
+++ gigasecond/src/main/kotlin/Gigasecond.kt
@@ -5,11 +5,9 @@ import java.time.temporal.ChronoUnit
 import java.time.temporal.Temporal
 import java.time.temporal.TemporalUnit
 
-class Gigasecond(private val instant: Temporal) {
+class Gigasecond(private val instant: LocalDateTime) {
 
+    constructor(_date: LocalDate): this(_date.atStartOfDay())
 
-    val date: LocalDateTime = when (instant) {
-        is LocalDate -> instant.atStartOfDay().plus(1000000000, ChronoUnit.SECONDS)
-        else -> LocalDateTime.from(instant.plus(1000000000, ChronoUnit.SECONDS))
-    }
+    val date: LocalDateTime = instant.plusSeconds(1000000000)
 }
