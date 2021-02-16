8d67c2fead9359b5542bf3c4de5c684c1993cf94
simopek <>
2021-02-16 21:59:39 +0100

    Add improved solution with regexp of run length encoding

--- run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
+++ run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
@@ -1,5 +1,7 @@
 object RunLengthEncoding {
 
+    /*
+
     fun encode(input: String): String {
 
         var output = StringBuilder()
@@ -59,4 +61,31 @@ object RunLengthEncoding {
 
         return output.toString()
     }
+
+     */
+
+
+    /*
+    With (.) we get any character. With \\1+ we refer to the first match, that
+    is any character, and we get the string that matches the repetition of the first match.
+
+    it is the match result
+    it.value is the matched string so we get the length and the first char
+     */
+    fun encode(input: String): String =
+        input.replace(Regex("(.)\\1+")) {
+            String.format("%d%s", it.value.length, it.value[0])
+        }
+
+    /*
+    With (\\d+)(.) we get any number followed by a character.
+    The matches are stored in the groups 1 and 2.
+    The group 2 keeps the character that we repeat by n times,
+    where n is group 1.
+     */
+    fun decode(input: String): String =
+        input.replace(Regex("(\\d+)(.)")) {
+            it.groupValues[2].repeat(it.groupValues[1].toInt())
+        }
+
 }
