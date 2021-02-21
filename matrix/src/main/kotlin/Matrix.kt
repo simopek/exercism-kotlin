5f5fde17f43e537f0a3f708a9d7bde2cbfd98d7f
simopek <>
2021-02-21 14:55:51 +0100

    Complete matrix exercise

--- matrix/src/main/kotlin/Matrix.kt
+++ matrix/src/main/kotlin/Matrix.kt
@@ -1,10 +1,23 @@
 class Matrix(private val matrixAsString: String) {
 
+    private val matrixRows: Map<Int, List<Int>>
+
+    init {
+
+        val matrixLines = matrixAsString.lines()
+
+        var rowIdx = 0
+        matrixRows = matrixLines.associateBy({rowIdx++},
+            { it -> it.replace("\\s+".toRegex(), " ")
+                .split(" ").map { it2 -> it2.trim() }.filter { it3 -> it3.isNotBlank() }
+                .map { it4 -> it4.toInt() }})
+    }
+
     fun column(colNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+        return matrixRows.values.map { row -> row[colNr - 1] }
     }
 
     fun row(rowNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+        return matrixRows[rowNr - 1]!!
     }
 }
